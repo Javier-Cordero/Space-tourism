d88a6e246f3793bc82456691607dca970fe66b95
Javier <javier.jc942@gmail.com>
2024-09-06 02:56:05 -0400

    destination

--- src/components/Destination.jsx
+++ src/components/Destination.jsx
@@ -17,12 +17,12 @@ export const Destination = () => {
           <div>
             <div className="flex justify-between w-64 absolute top-32 left-[63%]">
               {
-                destinations.map((num,item) => (<button key={item} onClick={() => handlePage(item+1)} className={`${item+1 === destinationPage ? 'border-b-2' : ''} py-2 uppercase text-white`}>{num.name}</button>))
+                destinations.map((num, item) => (<button key={item} onClick={() => handlePage(item + 1)} className={`${item + 1 === destinationPage ? 'border-b-2' : ''} py-2 uppercase text-white`}>{num.name}</button>))
               }
             </div>
-            <span className="absolute top-56 left-[63%] font-normal text-9xl text-white">{destinationItem.name}</span>
+            <span className="absolute top-52 left-[62%] font-normal text-9xl text-white">{destinationItem.name}</span>
             <p className="absolute top-[55%] left-[63%] w-96 text-slate-400">{destinationItem.description}</p>
-            <div className="h-1 w-96 bg-slate-400 absolute bottom-40 left-[63%]"></div>
+            <div className="h-1 w-96 absolute bottom-40 left-[63%] bg-[#ffffff1a]"></div>
             <span className="absolute bottom-32 left-[63%] font-medium text-slate-400">AVG DISTANCE</span>
             <span className="uppercase text-2xl absolute bottom-20 left-[63%] font-medium text-white">{destinationItem.distance}</span>
             <span className="absolute bottom-32 left-[80%] font-medium text-slate-400">EST. TRAVEL TIME</span>
